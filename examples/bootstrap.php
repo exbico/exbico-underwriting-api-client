ed30ee157132c04f09bd930e6b61057f7c23c920
Daniil Akhmetov <me@dan1.ru>
2021-03-09 13:49:42 +0500

    EXT-5846 #time 2h Add PHP client library examples

---
+++ examples/bootstrap.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Exbico\ApiSettings;
+use Exbico\Client;
+
+function getClient(): Client
+{
+    $apiSettings = new ApiSettings(getenv('API_TOKEN'));
+    $apiSettings->setBaseUrl(getenv('API_URL'));
+    return new Client($apiSettings);
+}
---
+++ examples/credit-rating-nbch.php
@@ -0,0 +1,33 @@
+<?php
+require_once __DIR__ . '/bootstrap.php';
+
+use Exbico\Api\V1\Dto\Request\DocumentDto;
+use Exbico\Api\V1\Dto\Request\PersonDto;
+
+$client = getClient();
+
+$document = new DocumentDto();
+$document->setNumber('333333');
+$document->setSeries('5555');
+
+$person = new PersonDto();
+$person->setFirstname('Иван');
+$person->setLastname('Иванов');
+$person->setMiddlename('Иванович');
+
+$reportStatus = $client->api()->creditRatingNbch()->requestReport($person, $document);
+printf("Credit rating NBCH requested with ID: %d\n", $reportStatus->getRequestId());
+printf("Waiting for status change\n");
+while ($reportStatus->getStatus() === 'inProgress') {
+    sleep(5);
+    $reportStatus = $client->api()->reportStatus()->getReportStatus($reportStatus->getRequestId());
+}
+
+if($reportStatus->getStatus() === 'success') {
+    printf("Start to download report\n");
+    $reportFilename = './reports/' . date('YmdHis') . '.pdf';
+    $client->api()->creditRatingNbch()->getPdfReport($reportStatus->getRequestId(), $reportFilename);
+    printf("Report downloaded: %s\n", $reportFilename);
+} else {
+    var_dump($reportStatus);
+}
