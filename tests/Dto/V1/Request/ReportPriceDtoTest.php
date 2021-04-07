a6d564ad3d5d0f79ac094c1e53d4f6e0a35c9d9b
Daniil Akhnetov <ahmetov@exbico.com>
2021-04-07 14:38:57 +0500

    EXT-5980 #time 30m add report price dto

---
+++ tests/Dto/V1/Request/ReportPriceDtoTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Exbico\Underwriting\Tests\Dto\V1\Request;
+
+use Exbico\Underwriting\Dto\AbstractDto;
+use Exbico\Underwriting\Dto\V1\Request\ReportPriceDto;
+use PHPUnit\Framework\TestCase;
+
+class ReportPriceDtoTest extends TestCase
+{
+    private const EXAMPLE_REPORT_TYPE = 'credit-rating-nbch';
+    private const EXAMPLE_LEAD_ID = 10;
+
+    public function testCreateWithoutConstructor(): void
+    {
+        $reportPrice = new ReportPriceDto();
+        $reportPrice->setReportType(self::EXAMPLE_REPORT_TYPE);
+        $reportPrice->setLeadId(self::EXAMPLE_LEAD_ID);
+        self::assertInstanceOf(AbstractDto::class, $reportPrice);
+        self::assertEquals(self::EXAMPLE_REPORT_TYPE, $reportPrice->getReportType());
+        self::assertEquals(self::EXAMPLE_LEAD_ID, $reportPrice->getLeadId());
+    }
+
+    public function testCreateViaConstructor(): void
+    {
+        $reportPrice = new ReportPriceDto([
+            'reportType' => self::EXAMPLE_REPORT_TYPE,
+            'leadId' => self::EXAMPLE_LEAD_ID,
+        ]);
+        self::assertInstanceOf(AbstractDto::class, $reportPrice);
+        self::assertEquals(self::EXAMPLE_REPORT_TYPE, $reportPrice->getReportType());
+        self::assertEquals(self::EXAMPLE_LEAD_ID, $reportPrice->getLeadId());
+    }
+}
