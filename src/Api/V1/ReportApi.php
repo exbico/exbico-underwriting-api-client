e944e82ffac761024e67bdccaa7a109757c255dc
Akhmetov Daniil <ahmetov@exbico.com>
2021-08-31 18:43:48 +0500

    EXT-6418 #time 5m fix exceptions

--- src/Api/V1/ReportApi.php
+++ src/Api/V1/ReportApi.php
@@ -3,12 +3,15 @@ declare(strict_types=1);
 
 namespace Exbico\Underwriting\Api\V1;
 
+use Exbico\Underwriting\Exception\BadRequestException;
+use Exbico\Underwriting\Exception\ForbiddenException;
 use Exbico\Underwriting\Exception\HttpException;
 use Exbico\Underwriting\Exception\LeadNotDistributedToContractException;
 use Exbico\Underwriting\Exception\NotEnoughMoneyException;
 use Exbico\Underwriting\Exception\ProductNotAvailableException;
 use Exbico\Underwriting\Exception\ReportGettingErrorException;
 use Exbico\Underwriting\Exception\ReportNotReadyException;
+use Exbico\Underwriting\Exception\ServerErrorException;
 
 abstract class ReportApi extends Api
 {
@@ -23,7 +26,7 @@ abstract class ReportApi extends Api
      */
     protected function checkNotEnoughMoney(HttpException $exception): void
     {
-        if ($exception->getCode() === NotEnoughMoneyException::HTTP_STATUS
+        if ($exception->getCode() === BadRequestException::HTTP_STATUS
             && $exception->getMessage() === self::MESSAGE_NOT_ENOUGH_MONEY) {
             throw new NotEnoughMoneyException($exception->getMessage(), $exception->getCode(), $exception);
         }
@@ -34,7 +37,7 @@ abstract class ReportApi extends Api
      */
     protected function checkProductIsAvailable(HttpException $exception): void
     {
-        if ($exception->getCode() === ProductNotAvailableException::HTTP_STATUS
+        if ($exception->getCode() === ForbiddenException::HTTP_STATUS
             && $exception->getMessage() === self::MESSAGE_PRODUCT_NOT_AVAILABLE) {
             throw new ProductNotAvailableException($exception->getMessage(), $exception->getCode(), $exception);
         }
@@ -46,7 +49,7 @@ abstract class ReportApi extends Api
      */
     protected function checkReportGettingError(HttpException $exception): void
     {
-        if ($exception->getCode() === ReportGettingErrorException::HTTP_STATUS
+        if ($exception->getCode() === ServerErrorException::HTTP_STATUS
             && $exception->getMessage() === self::MESSAGE_REPORT_GETTING_ERROR) {
             throw new ReportGettingErrorException($exception->getMessage(), $exception->getCode(), $exception);
         }
@@ -68,7 +71,7 @@ abstract class ReportApi extends Api
      */
     protected function checkForLeadNotDistributedToContract(HttpException $exception): void
     {
-        if ($exception->getCode() === LeadNotDistributedToContractException::HTTP_STATUS
+        if ($exception->getCode() === ForbiddenException::HTTP_STATUS
             && preg_match(self::PATTERN_LEAD_NOT_DISTRIBUTED_TO_CONTRACT, $exception->getMessage())) {
             throw new LeadNotDistributedToContractException($exception->getMessage());
         }
